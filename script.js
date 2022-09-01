5f5a3730280513437365cc7ba53ffa1d7177d158
Putra Syahwal Alam <50631694+akiraputra@users.noreply.github.com>
2022-09-01 14:53:27 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+import { library, icon } from "@fortawesome/fontawesome-svg-core";
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCamera);
+
+const camera = icon({ prefix: "fas", iconName: "camera" });
